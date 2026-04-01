276b4884d4f7124e19d82b9da480263bddfe34de
Paul Harriet Asiñero <harriebird@yahoo.com>
2026-04-01 17:39:17 +0800

    Display the generate_duration in the feedback lists

--- services/webapp/app/static/js/pages/assessinfo.js
+++ services/webapp/app/static/js/pages/assessinfo.js
@@ -113,7 +113,7 @@
                             </div>
                             <div class="d-flex w-100 align-items-start align-items-md-center justify-content-between flex-column flex-md-row">
                                 <div class="mb-1 small"><i class="bi bi-calendar-event"></i> Created: ${ new Date(feedback.created_at).toLocaleString() }</div>
-                                <div class="mb-1 small"><i class="bi bi-calendar-event"></i> Updated: ${ feedback.updated_at ? new Date(feedback.updated_at).toLocaleString() : "Not yet" }</div>
+                                <div class="mb-1 small"><i class="bi bi-clock"></i> Duration: ${ feedback.generate_duration ? feedback. generate_duration + " seconds": "Not recorded" }</div>
                                 <div class="mb-1 small"><i class="bi bi-database"></i> ID: ${ feedback.uuid }</div>
                             </div>
                         </a>
